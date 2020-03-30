5fa540d3de5c917891c770e21bc75356971b8afb
uxsasukexnaruto <nguyha16@gmail.com>
2020-03-30 10:51:52 -0700

    working on HoneyMakerBee.js

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototpye = Object.create(Bee.prototpye);
+HoneyMakerBee.prototpye.constructor = HoneyMakerBee;
+HoneyMakerBee.prototpye.makeHoney = function() {
+  this.honeyPot += 1;
+}
+HoneyMakerBee.prototpye.giveHoney = function() {
+  this.honeyPot -= 1;
+}
